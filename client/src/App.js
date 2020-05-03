ed7ea28e0a66262a90afb25dfc600f1523cfb8d7
Gaurav Personal <kinglygaurav@gmail.com>
2020-05-02 19:30:50 -0500

    Added missing env vars and SockJS nginx config... Ready for cloud

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <Link to="/otherpage">   Other Page! Yo!</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
